daa55efc55fef528e812a97c19df184362ac231a
Carlos Juarez <carlosgj94@gmail.com>
2024-06-05 13:07:58 +0200

    fix: fixing the navbar

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -7,12 +7,13 @@ import Link from "next/link";
 import { useState } from "react";
 import { MobileNavDialog } from "./mobileNavDialog";
 import { NavLink, type INavLink } from "./navLink";
+import { AvatarIcon, IconType } from "@aragon/ods";
 
 export const Navbar: React.FC = () => {
   const [open, setOpen] = useState(false);
 
   const navLinks: INavLink[] = [
-    { path: "/", id: "dashboard", name: "Dashboard", icon: undefined },
+    { path: "/", id: "dashboard", name: "Dashboard", icon: IconType.APP_DASHBOARD },
     ...plugins.map((p) => ({
       id: p.id,
       name: p.title,
@@ -23,44 +24,52 @@ export const Navbar: React.FC = () => {
 
   return (
     <>
-      <nav className="h-30 sticky top-0 w-full flex-col gap-2 border-b border-b-neutral-100 bg-neutral-0 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
-        <div className="flex w-full items-center justify-between">
-          <Link
-            href="/"
-            className={classNames(
-              "ml-10 flex items-center gap-x-5 rounded-full py-3 pb-5 md:rounded-lg",
-              "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
-            )}
-          >
-            <Image src="/logo-tk.svg" width="150" height="36" className="shrink-0" alt="Taiko" />
-          </Link>
-
-          <div className="flex items-center gap-x-2">
-            <div className="shrink-0">
-              <WalletContainer />
+      <nav className="h-30 sticky top-0 z-[var(--hub-navbar-z-index)] flex w-full items-center justify-center border-b border-b-neutral-100 bg-neutral-0">
+        <div className="w-full max-w-[1280px] flex-col gap-2 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
+          <div className="flex w-full items-center justify-between">
+            <div className="flex">
+              <Link
+                href="/"
+                className={classNames(
+                  "ml-10 flex items-center gap-x-5 rounded-full py-3 pb-5 md:rounded-lg",
+                  "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
+                )}
+              >
+                <Image src="/logo-tk.svg" width="164" height="32" className="shrink-0" alt="Taiko" />
+              </Link>
+              <div className="flex items-center gap-x-2 px-4">
+                <span className="text-sm leading-tight text-neutral-500">Powered by</span>
+                <Image src="/logo-aragon-bw-sm.png" width="24" height="24" alt="Aragon" />
+              </div>
             </div>
 
-            {/* Nav Trigger */}
-            <button
-              onClick={() => setOpen(true)}
-              className={classNames(
-                "rounded-full border border-neutral-100 bg-neutral-0 p-1 md:hidden",
-                "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
-              )}
-            >
-              {/*<AvatarIcon size="lg" icon={IconType.MENU} />*/}
-            </button>
-          </div>
-        </div>
+            <div className="flex items-center gap-x-2">
+              <div className="shrink-0">
+                <WalletContainer />
+              </div>
 
-        {/* Tab wrapper */}
-        <ul className="hidden gap-x-10 md:flex lg:pl-14">
-          {navLinks.map(({ id, name, path }) => (
-            <NavLink name={name} path={path} id={id} key={id} />
-          ))}
-        </ul>
+              {/* Nav Trigger */}
+              <button
+                onClick={() => setOpen(true)}
+                className={classNames(
+                  "rounded-full border border-neutral-100 bg-neutral-0 p-1 md:hidden",
+                  "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
+                )}
+              >
+                <AvatarIcon size="lg" icon={IconType.MENU} />
+              </button>
+            </div>
+          </div>
+
+          {/* Tab wrapper */}
+          <ul className="hidden gap-x-10 md:flex lg:pl-14">
+            {navLinks.map(({ id, name, path }) => (
+              <NavLink name={name} path={path} id={id} key={id} />
+            ))}
+          </ul>
+        </div>
       </nav>
-      {/*<MobileNavDialog open={open} navLinks={navLinks} onOpenChange={setOpen} />*/}
+      <MobileNavDialog open={open} navLinks={navLinks} onOpenChange={setOpen} />
     </>
   );
 };
