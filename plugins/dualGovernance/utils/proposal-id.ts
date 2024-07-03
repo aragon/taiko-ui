a7a4dcf0e99037499760a81cb9b9acca5cbdc65d
Jør∂¡ <itsbrickpop@gmail.com>
2024-07-03 15:23:22 +0200

    Correctly parsing proposal ID's

--- plugins/dualGovernance/utils/proposal-id.ts
+++ plugins/dualGovernance/utils/proposal-id.ts
@@ -1,8 +1,8 @@
 export function parseProposalId(proposalId: bigint) {
   const mask64Bit = BigInt("0xffffffffffffffff");
   const index = Number(proposalId & mask64Bit);
-  const startDate = proposalId & ((mask64Bit << BigInt(128)) >> BigInt(128));
-  const endDate = proposalId & ((mask64Bit << BigInt(64)) >> BigInt(64));
+  const startDate = Number((proposalId >> BigInt(128)) & mask64Bit);
+  const endDate = Number((proposalId >> BigInt(64)) & mask64Bit);
 
   return { index, startDate, endDate };
 }
