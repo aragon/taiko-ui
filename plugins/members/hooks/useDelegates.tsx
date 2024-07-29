dfa2eada6b41cd3245f34c40c08c5ea334baac16
Jør∂¡ <itsbrickpop@gmail.com>
2024-07-29 19:02:35 +0200

    Shuffling the list of delegate candidates

--- plugins/members/hooks/useDelegates.tsx
+++ plugins/members/hooks/useDelegates.tsx
@@ -1,19 +1,31 @@
-import { useReadContract } from "wagmi";
+import { useConfig } from "wagmi";
 import { DelegateAnnouncerAbi } from "@/plugins/members/artifacts/DelegationWall.sol";
-import { PUB_CHAIN, PUB_DELEGATION_WALL_CONTRACT_ADDRESS } from "@/constants";
+import { PUB_DELEGATION_WALL_CONTRACT_ADDRESS } from "@/constants";
+import { readContract } from "@wagmi/core";
+import { useQuery } from "@tanstack/react-query";
+import { useShuffled } from "@/hooks/useShuffled";
 
 /** Returns the list of delegates who posted a candidacy */
 export function useDelegates() {
-  const {
-    data: delegates,
-    status,
-    refetch,
-  } = useReadContract({
-    chainId: PUB_CHAIN.id,
-    address: PUB_DELEGATION_WALL_CONTRACT_ADDRESS,
-    abi: DelegateAnnouncerAbi,
-    functionName: "getCandidateAddresses",
-  });
+  const config = useConfig();
 
-  return { delegates, status, refetch };
+  const { data, status, refetch } = useQuery({
+    queryKey: ["delegate-addresses-list", PUB_DELEGATION_WALL_CONTRACT_ADDRESS],
+    queryFn: () => {
+      return readContract(config, {
+        abi: DelegateAnnouncerAbi,
+        address: PUB_DELEGATION_WALL_CONTRACT_ADDRESS,
+        functionName: "getCandidateAddresses",
+        args: [],
+      });
+    },
+    retry: true,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    retryOnMount: true,
+    staleTime: 1000 * 60 * 10,
+  });
+  const shuffledDelegates = useShuffled(data);
+
+  return { delegates: shuffledDelegates, status, refetch };
 }
